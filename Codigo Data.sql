4bd7d1433e50bda88c407c83c34c296bc69ac3b8
Mauricio rubio <rubiom831@gmail.com>
2024-04-24 08:36:09 -0600

    El script

---
+++ Codigo Data.sql
@@ -0,0 +1,51 @@
+CREATE DATABASE IF NOT EXISTS fisico;
+
+USE fisico;
+
+CREATE TABLE d_vendedor (
+  id_Vendedor INT PRIMARY KEY,
+  id_Usuario INT,
+  nombre VARCHAR(30),
+  apellido VARCHAR(30),
+  direccion VARCHAR(60)
+);
+
+CREATE TABLE h_venta (
+  id_Fecha INT PRIMARY KEY,
+  id_Vendedor INT,
+  id_Producto INT,
+  id_ModoPago INT,
+  id_Cliente INT
+);
+
+CREATE TABLE d_modopagos (
+  id_ModoPago INT PRIMARY KEY,
+  Nombre_ModoPago VARCHAR(20)
+);
+
+CREATE TABLE d_clientes (
+  id_Cliente INT PRIMARY KEY,
+  id_Usuario INT,
+  nombre VARCHAR(30),
+  apellido VARCHAR(30),
+  cedula VARCHAR(16),
+  historialdecompras TEXT
+);
+
+CREATE TABLE d_tiempo (
+  id_Fecha INT PRIMARY KEY,
+  Anyo INT,
+  mes INT,
+  desc_mes VARCHAR(20),
+  semana_anyo INT,
+  dia INT,
+  desc_dia VARCHAR(20),
+  hora INT,
+  minute INT
+);
+
+ALTER TABLE h_venta
+ADD CONSTRAINT fk_vendedor FOREIGN KEY (id_Vendedor) REFERENCES d_vendedor(id_Vendedor),
+ADD CONSTRAINT fk_modopago FOREIGN KEY (id_ModoPago) REFERENCES d_modopagos(id_ModoPago),
+ADD CONSTRAINT fk_cliente FOREIGN KEY (id_Cliente) REFERENCES d_clientes(id_Cliente),
+ADD CONSTRAINT fk_fecha FOREIGN KEY (id_Fecha) REFERENCES d_tiempo(id_Fecha);

09bb13fb18c1f37f9a3a03e5b208c0b55f20acc6
Mauricio rubio <rubiom831@gmail.com>
2024-05-20 18:55:55 -0600

    cantidad de producto en la Galeria

--- frontend-casadelpies/src/pages/Galeria.js
+++ frontend-casadelpies/src/pages/Galeria.js
@@ -4,41 +4,37 @@ import Header from '../components/Header';
 import { FaCartPlus } from "react-icons/fa6";
 import '../App.css';
 
-function Galeria({rol}) {
+function Galeria({ rol }) {
   const [productos, setProductos] = useState([]);
-
   const [searchQuery, setSearchQuery] = useState('');
 
   const handleSearchChange = (e) => {
     setSearchQuery(e.target.value);
   };
 
-  
   const filteredProductos = productos.filter((producto) => {
-    // Convierte los valores de los campos a minúsculas para realizar una búsqueda insensible a mayúsculas y minúsculas
-    const id_Producto = producto.id_Producto; 
+    const id_Producto = producto.id_Producto;
     const id_categoria = producto.id_categoria;
     const nombre = producto.nombre.toLowerCase();
     const descripcion = producto.descripcion.toLowerCase();
     const precio = producto.precio;
     const id_Marca = producto.id_Marca;
     const id_Promociones = producto.id_Promociones;
+    const cantidad = producto.cantidad; // Añadido cantidad
     const search = searchQuery.toLowerCase();
-    
-  
-    // Verifica si la cadena de búsqueda se encuentra en algún campo
+
     return (
-      id_Producto == (search) ||
-      id_categoria == (search) ||
+      id_Producto == search ||
+      id_categoria == search ||
       nombre.includes(search) ||
       descripcion.includes(search) ||
-      precio == (search) ||
-      id_Marca == (search) ||
-      id_Promociones == (search)
+      precio == search ||
+      id_Marca == search ||
+      id_Promociones == search ||
+      cantidad == search // Añadido cantidad al filtro
     );
   });
 
-  // Realiza una solicitud GET al servidor para obtener los productos
   useEffect(() => {
     fetch('http://localhost:5000/crud/readProductos')
       .then((response) => response.json())
@@ -46,48 +42,45 @@ function Galeria({rol}) {
       .catch((error) => console.error('Error al obtener los productos:', error));
   }, []);
 
-  return(
+  return (
     <div>
-      <Header rol={ rol} />
-      
+      <Header rol={rol} />
+
       <Container className="margen-contenedor">
+        <Row className="espaciado">
+          <Col sm="6" md="6" lg="4">
+            <FloatingLabel controlId="search" label="Buscar">
+              <Form.Control
+                type="text"
+                placeholder="Buscar"
+                value={searchQuery}
+                onChange={handleSearchChange}
+              />
+            </FloatingLabel>
+          </Col>
+        </Row>
 
-      <Row className="espaciado">
-        <Col sm="6" md="6" lg="4">
-          <FloatingLabel controlId="search" label="Buscar">
-            <Form.Control
-              type="text"
-              placeholder="Buscar"
-              value={searchQuery}
-              onChange={handleSearchChange}
-            />
-          </FloatingLabel>
-        </Col>
-      </Row>
-
-      <Row className="espaciado">
-        {productos.map((producto) => (
-          <Col sm="12" md="4" lg="3">
-            <Card>
-              <Card.Img className="image-card" variant="top" src={producto.imagen} alt={producto.nombre} />
-              <Card.Body>
-                <Card.Title>{producto.nombre}</Card.Title>
-                <Card.Text>
-                  {producto.descripcion}
-                </Card.Text>
-                <div>
-                  <Badge bg="success">PrecioC$: {producto.precio}</Badge>
-                </div>
-              </Card.Body>
-              <Card.Body>
-                <button href="#"><FaCartPlus /></button>
-              </Card.Body>
-            </Card>
-          </Col>            
-        ))}
-      </Row>
-    </Container>
-
+        <Row className="espaciado">
+          {filteredProductos.map((producto) => (
+            <Col key={producto.id_Producto} sm="12" md="4" lg="3">
+              <Card>
+                <Card.Img className="image-card" variant="top" src={producto.imagen} alt={producto.nombre} />
+                <Card.Body>
+                  <Card.Title>{producto.nombre}</Card.Title>
+                  <Card.Text>{producto.descripcion}</Card.Text>
+                  <div>
+                    <Badge bg="success">PrecioC$: {producto.precio}</Badge>
+                    <Badge bg="info" className="ms-2">Cantidad: {producto.cantidad}</Badge>
+                  </div>
+                </Card.Body>
+                <Card.Body>
+                  <button href="#"><FaCartPlus /></button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 }
